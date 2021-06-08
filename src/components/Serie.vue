2a697a3624695893e3ed3bfd4a8042eb1220d06d
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-06-08 14:26:13 +0200

    Aggiunta dettagli in series

---
+++ src/components/Serie.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="card">
+       <div class="img-cont">
+           <img :src="singleProduct.thumb" :alt="singleProduct.series">
+       </div>
+       <div class="text">{{singleProduct.series}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Serie",
+    props: ["singleProduct"]
+}
+</script>
+<style lang="scss">
+    .card {
+        width: calc(100% / 6);
+        padding:10px;
+        .img-cont{
+          height:180px;
+          overflow: hidden; 
+        }
+        .text{
+            padding:20px 0px;
+            font-size:15px;
+            color:white;
+            text-transform: uppercase;
+        }
+    }
+</style>
--- src/components/Series.vue
+++ src/components/Series.vue
@@ -4,16 +4,34 @@
             <span class="btn-current">
                 Current Series
             </span>
+            <div class="series-cont">
+                <Serie 
+                    v-for="(serie, index) in series"
+                    :key="index"
+                    :single-product="serie"
+                /> 
             <span class="btn-load">
                 Load More
-            </span>
+            </span> 
+            </div>
+            
         </div>
     </section>
 </template>
 
 <script>
+import Serie from './Serie';
+import cardser from '../data/cardser';
 export default {
-    name:"Series"
+    name:"Series",
+    components: {
+       Serie
+    },
+    data: function() {
+        return {
+            series: cardser
+        }
+    }
 }
 </script>
 
@@ -21,8 +39,13 @@ export default {
 @import "../style/variables";
 section{
     position:relative;
-    height:600px;
     background-color: #1C1C1C;
+}
+.series-cont{
+    padding:60px 0px;
+    position:relative;
+    display: flex;
+    flex-wrap: wrap;
 }
 .btn-current{
     position:absolute;
@@ -37,7 +60,9 @@ section{
 .btn-load{
     position:absolute;
     right:50%;
-    bottom:4%;
+    bottom:2%;
+    transform: translateX(50%);
+    font-size: 13px;
     font-weight: 700;
     text-transform: uppercase;
     padding:10px 50px;
